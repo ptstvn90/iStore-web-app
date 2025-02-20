ee63ad373d7ed6af37540b6525a01d9b72a42f36
ptstvn90 <84872607+ptstvn90@users.noreply.github.com>
2025-02-20 10:27:30 +0200

    Update routing.js

--- src/router/routing.js
+++ src/router/routing.js
@@ -1,13 +1,23 @@
+// Common Comps
+
 export { Layout } from "../comps/common/Layout";
+
+// Pages Comps
+
 export { Home } from "../screen/Home/Home";
+export { CartPage } from "../screen/CartPage/CartPage";
+export { AboutApple } from "../screen/About/AboutApple";
+export { AppleSupport } from "../screen/Support/AppleSupport";
+
+// Product Comps
+
 export { Promo } from "../comps/product/PromoItems";
 export { Product } from "../comps/product/Product";
 export { ProductCard } from "../comps/product/ProductCard";
 export { ShippingInformation } from "../comps/product/ShippingInformation";
 export { ProductBanner } from "../comps/product/ProductBanner";
 export { OwnerShipPosts } from "../comps/product/OwnerShipPosts";
-export { CartPage } from "../screen/CartPage/CartPage";
-export { AboutApple } from "../screen/About/AboutApple";
-export { AppleSupport } from "../screen/Support/AppleSupport";
+
+
 
 
